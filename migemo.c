f2a3282acf16aa4de2ea79491b0a8b914b6353f0
Shuhei Tanuma <stanuma@zynga.co.jp>
2011-01-11 15:54:15 +0900

    コンストラクタを追加した

--- migemo.c
+++ migemo.c
@@ -1,12 +1,47 @@
 #include "php_migemo.h"
 
+typedef struct{
+    zend_object zo;
+    migemo *migemo;
+} php_migemo;
+
+static void php_migemo_free_storage(php_migemo *object TSRMLS_DC)
+{
+    zend_object_std_dtor(&object->zo TSRMLS_CC);
+    
+    if(!object->migemo){
+      migemo_free(object->migemo);
+    }
+    object->migemo = NULL;
+    efree(object);
+}
+
+zend_object_value php_migemo_new(zend_class_entry *ce TSRMLS_DC)
+{
+	zend_object_value retval;
+	php_migemo *obj;
+	zval *tmp;
+
+	obj = ecalloc(1, sizeof(*obj));
+	zend_object_std_init( &obj->zo, ce TSRMLS_CC );
+	zend_hash_copy(obj->zo.properties, &ce->default_properties, (copy_ctor_func_t) zval_add_ref, (void *) &tmp, sizeof(zval *));
+
+	retval.handle = zend_objects_store_put(obj, 
+        (zend_objects_store_dtor_t)zend_objects_destroy_object,
+        (zend_objects_free_object_storage_t)php_migemo_free_storage,
+        NULL TSRMLS_CC);
+	retval.handlers = zend_get_std_object_handlers();
+	return retval;
+}
+
+
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_migemo_query, 0, 0, 1)
     ZEND_ARG_INFO(0, query)
 ZEND_END_ARG_INFO()
 PHP_METHOD(migemo, query)
 {
-    migemo *m;
+    php_migemo *this = (php_migemo *) zend_object_store_get_object(getThis() TSRMLS_CC);
     char *query;
     int ret = 0;
     int query_len = 0;
@@ -16,16 +51,21 @@ PHP_METHOD(migemo, query)
         return;
     }
 
-    m =  migemo_open("/usr/local/share/migemo/utf-8/migemo-dict");
-    result = migemo_query(m, query);
+    result = migemo_query(this->migemo, query);
 
     RETVAL_STRING(result, 1);
-    migemo_release(m, result);
-    migemo_close(m);
+    migemo_release(this->migemo, result);
+}
+
+PHP_METHOD(migemo, __construct){
+    php_migemo *this = (php_migemo *) zend_object_store_get_object(getThis() TSRMLS_CC);
+
+    this->migemo = migemo_open("/usr/local/share/migemo/utf-8/migemo-dict");
 }
 
 
 PHPAPI function_entry php_migemo_methods[] = {
+    PHP_ME(migemo, __construct, NULL, ZEND_ACC_PUBLIC)
     PHP_ME(migemo, query, arginfo_migemo_query, ZEND_ACC_PUBLIC)
     {NULL, NULL, NULL}
 };
@@ -34,6 +74,7 @@ PHP_MINIT_FUNCTION(migemo) {
     zend_class_entry ce;
     INIT_CLASS_ENTRY(ce, "Migemo", php_migemo_methods);
     migemo_class_entry = zend_register_internal_class(&ce TSRMLS_CC);
+  	migemo_class_entry->create_object = php_migemo_new;
 
     return SUCCESS;
 }
